b6ea9e9cac50f4f0c7561afbc8b640d31e7ed49d
NguyenDuybao <duybao.dragon@gmail.com>
2021-07-21 22:41:41 +0700

    httptest

--- tests/Feature/ExampleHttpTest.php
+++ tests/Feature/ExampleHttpTest.php
@@ -8,9 +8,38 @@ use Illuminate\Foundation\Testing\WithFaker;
 
 class ExampleTest extends TestCase
 {
-    use WithFaker;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
 
-    
+        $response->assertStatus(200);
+    }
+
+
+    public function testUserCreationEndpointTest()
+    {
+        $name = $this->faker->name();
+        $email = $this->faker->email();
+        $password = "mypassword";
+
+        $response = $this->postJson('/api/createuser', [
+            'name' => $name, 
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password
+        ]); 
+
+        $response
+            ->assertStatus(200)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
+    }
        
 
 }
