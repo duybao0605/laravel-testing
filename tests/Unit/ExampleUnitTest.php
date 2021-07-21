5afc96e67fe51458b7f25b3f8b54f304ce8f8917
NguyenDuybao <duybao.dragon@gmail.com>
2021-07-21 22:33:11 +0700

    change unittest

--- tests/Unit/ExampleUnitTest.php
+++ tests/Unit/ExampleUnitTest.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class ExampleTest extends TestCase
 {
@@ -13,6 +15,31 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $this->assertTrue(true);
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
     }
+
+
+    public function testUserCreationEndpointTest()
+    {
+        $name = $this->faker->name();
+        $email = $this->faker->email();
+        $password = "mypassword";
+
+        $response = $this->postJson('/api/createuser', [
+            'name' => $name, 
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password
+        ]); 
+
+        $response
+            ->assertStatus(201)
+            ->assertExactJson([
+                'message' => "Successfully created user!",
+            ]);
+    }
+       
+
 }
