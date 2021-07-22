936eb9d48397b7a13892fa2861f04b41fc4b5caa
NguyenDuybao <duybao.dragon@gmail.com>
2021-07-22 14:06:19 +0700

    unittest default

--- tests/Unit/ExampleUnitTest.php
+++ tests/Unit/ExampleUnitTest.php
@@ -19,14 +19,5 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testUserCreation()
-    {
-        $user = new User([
-            'name' => "Test User",
-            'email' => "test@mail.com",
-            'password' => bcrypt("testpassword")
-        ]);   
-
-        $this->assertEquals('Test User', $user->name);
-    }
+    
 }
