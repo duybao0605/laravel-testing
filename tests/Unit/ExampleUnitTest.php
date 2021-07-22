40d79d9757c4d5bf8cf6f3d3e297f5f93d9bc2ad
NguyenDuybao <duybao.dragon@gmail.com>
2021-07-22 11:04:09 +0700

    add http config ,file examplehttp, file exunit

--- tests/Unit/ExampleUnitTest.php
+++ tests/Unit/ExampleUnitTest.php
@@ -7,3 +7,26 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\User;
 
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCreation()
+    {
+        $user = new User([
+            'name' => "Test User",
+            'email' => "test@mail.com",
+            'password' => bcrypt("testpassword")
+        ]);   
+
+        $this->assertEquals('Test User', $user->name);
+    }
+}
